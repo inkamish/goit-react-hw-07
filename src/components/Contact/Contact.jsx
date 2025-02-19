dde96b8f4e1305d8edf03467b0bca0bf912766f6
inna_mishchenko <mishhenko.inna@gmail.com>
2025-02-19 17:09:39 +0100

    fix loader and disable buttons during loading

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,27 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { deleteContact } from "../../redux/contactsOps";
 import styles from "./Contact.module.css";
+import { selectIsLoading } from "../../redux/selectors";
 
 const Contact = ({ contact: { id, name, number } }) => {
   const dispatch = useDispatch();
 
   const handleDelete = () => {
-    dispatch(deleteContact(id)); // Видаляє контакт за id
+    dispatch(deleteContact(id));
   };
+
+  const isLoading = useSelector(selectIsLoading);
 
   return (
     <div className={styles.contact}>
       <p>{name}</p>
       <p>{number}</p>
-      <button className={styles.btn} type="button" onClick={handleDelete}>
+      <button
+        className={styles.btn}
+        type="button"
+        onClick={handleDelete}
+        disabled={isLoading}
+      >
         Delete
       </button>
     </div>
